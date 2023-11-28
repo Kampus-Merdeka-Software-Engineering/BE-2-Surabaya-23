c3aabd2388d1585dca17ec8c44fa709408b1b03a
TrioHermanto <dcom1084@gmail.com>
2023-11-28 22:30:24 +0700

    Handling Routes and Request with Expressjs

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+function LoggerMiddleware(req, res, next){
+    console.log(`Reques api recived at: ${new Date()}`)
+    next();
+}
+
+app.use(LoggerMiddleware);
+
 //endpoint get all customers
 app.get('/customers', (req, res) => {
+    const { keyword, search } = req.query;
+    let data = [
+        {
+            name: 'Trio Hermanto',
+            email: 'triohermanto@gmail.com',
+            role: 'beban'
+        },
+        {
+            name: 'Hermanto',
+            email: 'hermanto@gmail.com',
+            role: 'test'
+        }
+    ]
+    
+    if(search){
+        data.map(value => {
+            value.search = search
+            return value
+        });
+    }
+
+    console.log('keyword: ', keyword)
+    console.log('search: ', search )
+
     res.json({
         message: 'Get list all data customers is success',
-        data: [
-            {
-                name: 'Trio Hermanto',
-                email: 'triohermanto@gmail.com',
-                role: 'beban'
-            },
-            {
-                name: 'Hermanto',
-                email: 'hermanto@gmail.com',
-                role: 'test'
-            }
-        ]
+        data: data
     })
 })
 
 //endpoint get detail customers by id
 app.get('/customers/:id', (req, res) => {
+    const customerId = req.params.id;
     res.json({
-        message: 'Get detail data customers is success',
+        message: `Get detail data customers by id ${customerId} success`,
         data:
             {
+                id: 'customerId',
                 name: 'Trio Hermanto',
                 email: 'triohermanto@gmail.com',
                 role: 'beban'
@@ -40,13 +65,15 @@ app.get('/customers/:id', (req, res) => {
 
 //endpoint post customers
 app.post('/customers', (req, res) => {
+    const { name, email, role } = req.body;
+
     res.json({
         message: 'Create data customers success',
         data: 
             {
-                name: 'Trio Hermanto',
-                email: 'triohermanto@gmail.com',
-                role: 'beban',
+                name: name,
+                email: email,
+                role: role,
                 created_at: '2023/11/27 20:30:00',
                 updates_at: '2023/11/27 20:30:00'
             }
@@ -55,10 +82,12 @@ app.post('/customers', (req, res) => {
 
 //endpoint update customers by id
 app.put('/customers/:id', (req, res) => {
+    const customerId = req.params.id;
     res.json({
-        message: 'Create data customers success',
+        message: `Update data customers by id ${customerId} success`,
         data: 
             {
+                id: 'customerId',
                 name: 'Trio tes',
                 email: 'tes@gmail.com',
                 role: 'beban tes',
@@ -70,8 +99,9 @@ app.put('/customers/:id', (req, res) => {
 
 //endpoint delete by id
 app.delete('/customers/:id', (req, res) => {
+    const customerId = req.params.id;
     res.json({
-        message: 'Delete data customer by id $id success',
+        message: `Delete data customer by id ${customerId} success`,
         data: {}
     })
 })
