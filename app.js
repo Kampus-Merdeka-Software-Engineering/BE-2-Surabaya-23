492445fc7fc0c338da53df839bd06bd11ad43a22
thomaseka <thomaswastika@gmail.com>
2023-12-03 01:22:06 +0800

    initialize express  app

--- app.js
+++ app.js
@@ -1,111 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+
 const app = express();
-const port = 3000;
+const router = express.Router();
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
+router.get("/hi", (req, res) => {
+  res.send("Hello");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(router);
 
-function LoggerMiddleware(req, res, next){
-    console.log(`Reques api recived at: ${new Date()}`)
-    next();
-}
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
-app.use(LoggerMiddleware);
-
-//endpoint get all customers
-app.get('/customers', (req, res) => {
-    const { keyword, search } = req.query;
-    let data = [
-        {
-            name: 'Trio Hermanto',
-            email: 'triohermanto@gmail.com',
-            role: 'beban'
-        },
-        {
-            name: 'Hermanto',
-            email: 'hermanto@gmail.com',
-            role: 'test'
-        }
-    ]
-    
-    if(search){
-        data.map(value => {
-            value.search = search
-            return value
-        });
-    }
-
-    console.log('keyword: ', keyword)
-    console.log('search: ', search )
-
-    res.json({
-        message: 'Get list all data customers is success',
-        data: data
-    })
-})
-
-//endpoint get detail customers by id
-app.get('/customers/:id', (req, res) => {
-    const customerId = req.params.id;
-    res.json({
-        message: `Get detail data customers by id ${customerId} success`,
-        data:
-            {
-                id: 'customerId',
-                name: 'Trio Hermanto',
-                email: 'triohermanto@gmail.com',
-                role: 'beban'
-            }
-    })
-})
-
-//endpoint post customers
-app.post('/customers', (req, res) => {
-    const { name, email, role } = req.body;
-
-    res.json({
-        message: 'Create data customers success',
-        data: 
-            {
-                name: name,
-                email: email,
-                role: role,
-                created_at: '2023/11/27 20:30:00',
-                updates_at: '2023/11/27 20:30:00'
-            }
-    })
-})
-
-//endpoint update customers by id
-app.put('/customers/:id', (req, res) => {
-    const customerId = req.params.id;
-    res.json({
-        message: `Update data customers by id ${customerId} success`,
-        data: 
-            {
-                id: 'customerId',
-                name: 'Trio tes',
-                email: 'tes@gmail.com',
-                role: 'beban tes',
-                created_at: '2023/11/27 20:30:00',
-                updates_at: '2023/11/27 20:40:00'
-            }
-    })
-})
-
-//endpoint delete by id
-app.delete('/customers/:id', (req, res) => {
-    const customerId = req.params.id;
-    res.json({
-        message: `Delete data customer by id ${customerId} success`,
-        data: {}
-    })
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+//const bodyParser = require("body-parser");
+//app.use(bodyParser.json());
