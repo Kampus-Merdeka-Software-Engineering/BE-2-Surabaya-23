012d3f20873675f441f853aea59b451d5dd9de94
thomaseka <thomaswastika@gmail.com>
2023-12-04 06:50:26 +0800

    newest update review and menu function

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 
+const cors = require("cors");
 const app = express();
-const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const menuRoutes = require("./routes/menuRoutes");
 
-app.use(router);
 app.use(menuRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
 //const bodyParser = require("body-parser");
 //app.use(bodyParser.json());
+
+// const router = express.Router();
+// app.use(router);
