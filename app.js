670e8a5deb4ffc139b0b4a971d271a990b23ffad
thomaseka <thomaswastika@gmail.com>
2023-12-07 01:31:55 +0800

    delete the useless code

--- app.js
+++ app.js
@@ -18,9 +18,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-//const bodyParser = require("body-parser");
-//app.use(bodyParser.json());
-
-// const router = express.Router();
-// app.use(router);
